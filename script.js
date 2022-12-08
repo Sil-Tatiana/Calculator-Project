d31a052e4cea4f098f8be306d30628b3172a4dae
Silvia Neves <siltatiana.sn@gmail.com>
2022-12-08 15:16:37 +0000

    selects all the buttons elements

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// create an add function
+// select all the buttons/text outputs
+const btnNumbers = document.getElementsByClassName("num-btn");
+const btnOperations = document.getElementsByClassName("btn-operation");
+const btnEquals = document.getElementsByClassName("btn-equals");
+const btnClear = document.getElementsByClassName("btn-clear");
+const btnDot = document.getElementsByClassName("btn-dot");
+const initialCalcText = document.getElementsByClassName("initial-calc");
+const currentCalcText = document.getElementsByClassName("current-calc");
 
+// create an add function
 // create an subtraction function
 // create an division function
 // create an multiplication function
