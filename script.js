3af208f2e2c5db96118b757cc4d93691df6153c5
Silvia Neves <siltatiana.sn@gmail.com>
2022-12-09 13:21:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // select all the buttons/text outputs
+const allButtons = document.querySelector(".keys-btn");
 const btnNumbers = document.getElementsByClassName("num-btn");
 const btnOperations = document.getElementsByClassName("btn-operation");
 const btnEquals = document.getElementsByClassName("btn-equals");
 const btnClear = document.getElementsByClassName("btn-clear");
 const btnDot = document.getElementsByClassName("btn-dot");
-const initialCalcText = document.getElementsByClassName("initial-calc");
-const currentCalcText = document.getElementsByClassName("current-calc");
 
+allButtons.addEventListener("click", (event) => {
+  console.log(event.target);
+});
 // create an add function
 // create an subtraction function
 // create an division function
