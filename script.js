bed78c6701edfef37dee86c4b2d41851b66028a6
Silvia Neves <siltatiana.sn@gmail.com>
2022-12-09 18:15:41 +0000

    refactors functions

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 // select all the buttons/text outputs
-const allButtons = document.querySelector(".keys-btn");
-const btnNumbers = document.getElementsByClassName("num-btn");
+// const allButtons = document.querySelector(".keys-btn");
+const btnNumbers = document.querySelectorAll(".num-btn");
 const btnOperations = document.getElementsByClassName("btn-operation");
 const btnEquals = document.getElementsByClassName("btn-equals");
 const btnClear = document.getElementsByClassName("btn-clear");
 const btnDot = document.getElementsByClassName("btn-dot");
 const display = document.querySelector(".display");
 
-allButtons.addEventListener("click", (event) => {
-  const singleBtn = event.target;
-  // console.log(singleBtn);
-  const btnValue = singleBtn.textContent;
-  const displayValue = display.textContent;
-
-  if (displayValue === "0") {
-    display.textContent = btnValue;
-  } else {
-    display.textContent = displayValue + btnValue;
-  }
-});
-// create an add function
-// create an subtraction function
-// create an division function
-// create an multiplication function
-//create a delete action
+function getNumberValue() {
+  btnNumbers.forEach((number) =>
+    number.addEventListener("click", function (event) {
+      const singleButton = event.target;
+      const btnValue = singleButton.textContent;
+      const displayValue = Number(display.textContent);
+      if (displayValue === 0) {
+        display.textContent = btnValue;
+      } else {
+        display.textContent = displayValue + btnValue;
+      }
+    })
+  );
+}
+getNumberValue();
